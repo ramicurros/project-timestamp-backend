f2867adb1e455f97660c6e720b1ee30b5af56e30
ramicurros <ramicurros@gmail.com>
2023-04-25 15:35:48 -0300

    first commit

--- index.js
+++ index.js
@@ -5,10 +5,33 @@
 var express = require('express');
 var app = express();
 
+app.get("/api/:date?", (req, res) => {
+  if (req.params.date) {
+    if (typeof req.params.date === 'string') {
+      let dateObj = new Date(req.params.date);
+      if (dateObj instanceof Date && !isNaN(x)) {
+        res.json({ unix: Math.floor(Date.parse(dateObj.toString()) / 1000), utc: dateObj.toString() })
+      } else {
+        res.json({ error: "Invalid Date" })
+      }
+    }
+    if (req.params.date instanceof Date && !isNaN(x)) {
+      res.json({ unix: Math.floor(Date.parse(req.params.date.toString()) / 1000), utc: req.params.date.toString() })
+    } else {
+      res.json({ error: "Invalid Date" })
+    }
+  } else {
+    let unixNow = Math.floor(Date.now() / 1000);
+    let now = new Date().toString();
+    res.json({ unix: unixNow, utc: now });
+  }   
+
+})
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,7 +44,7 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
 
